343dda4810165c976a5cfdf8cc9d8d804be5546d
zachlengel <156560805+zachlengel@users.noreply.github.com>
2024-04-17 15:18:00 -0400

    added index and show actions to plants

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,12 @@
 class PlantsController < ApplicationController
+
+  def index
+    @plants = Plant.all
+    render :index
+  end
+
+  def show
+    @plant = Plant.find_by(id: params[:id])
+    render :show
+  end
 end
